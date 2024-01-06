03f896bfc1975e70895ab399db2fb9a3ef9380d9
Yang Si Jun <aersijun@gmail.com>
2024-01-07 02:17:47 +0800

    longest common prefix

---
+++ easy_python/longest_common_prefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for string in strs[1:]:
+            while string.find(prefix) != 0:
+                prefix = prefix[:-1] #removes last letter
+                if not prefix:
+                    return ""
+        return prefix
+
+
